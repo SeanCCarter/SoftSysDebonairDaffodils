c45bdd8f5f93c0fe21cc9be65f85fda47a7d0c53
Matt Ruehle <matthew.t.ruehle@gmail.com>
2017-04-30 23:35:43 -0400

    Functions, code cleanup, etc.

--- sdl_test.c
+++ sdl_test.c
@@ -21,6 +21,8 @@
 #include <string.h>
 #include <stdint.h>
 
+#include <unistd.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -32,6 +34,7 @@ int CANVAS_BYTES; // should get set to X*Y*depth
 int STRIDE; // for ease of calculation later; ideally X*depth?..
 
 char C_WHITE = 0xFF;
+char SAVE_AS[80] = "saved.image"; // max filename length of 80 for now.
 
 int min_of(int a, int b) {
 	// utility for getting the lower of two options.
@@ -74,6 +77,17 @@ Tool* make_tool() {
 	return t;
 }
 
+void write_pixel_value(SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
+	// writes r, g, b to location x, y on surface. Might mix up RGB vs ARGB, BGR, etc.
+	int depth = surface->format->BytesPerPixel;
+	int offset = y * (surface->pitch) + x * depth;
+	uint8_t* p = (uint8_t *) surface->pixels;
+	// uint8_t* p = (uint8_t) surface->pixels + y * surface->pitch + x * depth;
+	p[offset + 0] = b;
+	p[offset + 1] = g;
+	p[offset + 2] = r;
+}
+
 void draw(SDL_Surface *surface, int x, int y, Tool* t) {
 	// draws a... "circle" (actually a square), within the boundaries.
 	uint8_t r = t->r;
@@ -83,9 +97,9 @@ void draw(SDL_Surface *surface, int x, int y, Tool* t) {
 	int i, j;
 
 	int startx = max_of(x - t->radius, 0);
-	int endx = min_of(x + t->radius, CANVAS_XWIDTH);
+	int endx = min_of(x + t->radius, CANVAS_XWIDTH-1);
 	int starty = max_of(y - t->radius, 0);
-	int endy = min_of(y + t->radius, CANVAS_YWIDTH);
+	int endy = min_of(y + t->radius, CANVAS_YWIDTH-1);
 
 	for (i = startx; i <= endx; i++) {
 		for (j = starty; j <= endy; j++) { // <= or <  -- todo: check.
@@ -94,16 +108,12 @@ void draw(SDL_Surface *surface, int x, int y, Tool* t) {
 	}
 }
 
-
-
 void print_pixel(Pixel* p) {
 	// utility for printing the values of a Pixel*.
 	printf("a:%u\tr:%u\tg:%u\tb:%u\n", p->a, p->r, p->g, p->b);
 }
 
-
 typedef enum {false, true} bool; // utility because i like true/false.
-
 
 Pixel* get_pixel_value(SDL_Surface *surface, int x, int y) {
 	// Queries a given surface at x and y, returning the values of the pixel stored there as a Pixel.
@@ -118,18 +128,6 @@ Pixel* get_pixel_value(SDL_Surface *surface, int x, int y) {
 	return res; // be sure to free() the pixel later.
 }
 
-void write_pixel_value(SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
-	// writes r, g, b to location x, y on surface. Might mix up RGB vs ARGB, BGR, etc.
-	int depth = surface->format->BytesPerPixel;
-	int offset = y * (surface->pitch) + x * depth;
-	uint8_t* p = (uint8_t *) surface->pixels;
-	// uint8_t* p = (uint8_t) surface->pixels + y * surface->pitch + x * depth;
-	p[offset + 0] = b;
-	p[offset + 1] = g;
-	p[offset + 2] = r;
-}
-
-
 void save_as(SDL_Surface *surface, char* filename) {
 	int w = surface->w;
 	int h = surface->h;
@@ -141,11 +139,6 @@ void save_as(SDL_Surface *surface, char* filename) {
 
 	FILE* fd = fopen(filename, "w");
 
-	// for now, disregarding w, h, and pitch.
-	// fprintf(fd, "%i\n", w);
-	// fprintf(fd, "%i\n", h);
-	// fprintf(fd, "%i\n", pitch);
-	// fprintf(fd, "%d\n", p);
 	fwrite(p, sizeof(uint8_t), w*h*depth, fd);
 	fclose(fd);
 }
@@ -165,15 +158,108 @@ void load_as(SDL_Surface *surface, char* filename) {
 
 	fread(p, sizeof(uint8_t), w*h*depth, fd);
 	fclose(fd);
+}
 
 
+void parse_start_args(int argc, char* argv[], bool* to_clear) {
+	int option = 0;
+	while ((option = getopt(argc, argv,"x:y:f:hc")) != -1) {
+		switch (option) {
+			case 'x':
+				CANVAS_XWIDTH = atoi(optarg);
+				break;
+			case 'y':
+				CANVAS_YWIDTH = atoi(optarg);
+				break;
+			case 'f':
+				strcpy(SAVE_AS, optarg);
+				break;
+			case 'h':
+				printf("Paint program in C.\n\t-x: x-width\n\t-y: y-width\n\t-f: filename to load/save as\n\t-c: clear image\n\t-h: this help\n");
+				exit(0);
+				break;
+			case 'c':
+				*to_clear = true;
+				break;
+			default:
+				printf("Unrecognized flag.\n");
+				printf("-x: x-width\n\t-y: y-width\n\t-f: filename to load/save as\n\t-c: clear image\n\t-h: help\n");
+				exit(0);
+				break;
+		}
+	}
+	CANVAS_BYTES = CANVAS_XWIDTH*CANVAS_YWIDTH*CANVAS_DEPTH; // total size of canvas
+	STRIDE = CANVAS_XWIDTH*CANVAS_DEPTH;
+}
 
+
+void process_arrow_key(int* cx, int* cy, SDL_Event e) {
+	// Processes arrow keys, changing the values at cx and cy accordingly.
+	switch (e.key.keysym.sym) {
+		case SDLK_UP:
+			if ((*cy) > 0) {
+				(*cy)--;
+			}
+			break;
+		case SDLK_DOWN:
+			if ((*cy) < (CANVAS_YWIDTH - 1)) {
+				(*cy)++;
+			}
+			break;
+		case SDLK_LEFT:
+			if ((*cx) > 0) {
+				(*cx)--;
+			}
+			break;
+		case SDLK_RIGHT:
+			if ((*cx) < (CANVAS_XWIDTH - 1)) {
+				(*cx)++;
+			}
+			break;
+		default:
+			printf("Unrecognized arrow key.\n");
+			break;
+	}
+}
+
+
+void process_color_key(Tool* user_tool, SDL_Event e) {
+	switch (e.key.keysym.sym) {
+		case SDLK_r:
+			user_tool->r = 255;
+			user_tool->g = 0;
+			user_tool->b = 0;
+			break;
+		case SDLK_k:
+			user_tool->r = 0;
+			user_tool->g = 0;
+			user_tool->b = 0;
+			break;
+		case SDLK_g:
+			user_tool->r = 0;
+			user_tool->g = 255;
+			user_tool->b = 0;
+			break;
+		case SDLK_b:
+			user_tool->r = 0;
+			user_tool->g = 0;
+			user_tool->b = 255;
+			break;
+		case SDLK_w:
+			user_tool->r = 255;
+			user_tool->g = 255;
+			user_tool->b = 255;
+			break;
+		default:
+			printf("Unrecognized color key.\n");
+			break;
+	}
 }
 
 
 int main(int argc, char* argv[]) {
-
-	char* filename = "saved.image";
+	bool to_clear = false;
+	parse_start_args(argc, argv, &to_clear);
 
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;  // not sure if NULL declaration is necessary.
@@ -210,6 +296,17 @@ int main(int argc, char* argv[]) {
 
 	SDL_FillRect(canvas, NULL, SDL_MapRGB(canvas->format, 0xFF, 0xFF, 0xFF)); // is 0xFF the proper syntax here?
 	
+	// checks if SAVE_AS exists; if it does, load it automatically.
+	if (!to_clear) {
+		FILE *sa = fopen(SAVE_AS,"r");
+		if (sa != NULL) {
+			fclose(sa);
+			printf("Loading %s...\n", SAVE_AS);
+			load_as(canvas, SAVE_AS);
+		}
+	}
+	// is this a memory leak? should we free() sa?
+
 	SDL_UpdateWindowSurface(window);
 
 	bool run_flag = true;
@@ -227,49 +324,17 @@ int main(int argc, char* argv[]) {
 
 				switch (event.key.keysym.sym) {
 					case SDLK_UP:
-						if (cursor_y > 0) {
-							cursor_y--;
-						}
-						break;
 					case SDLK_DOWN:
-						if (cursor_y < (CANVAS_YWIDTH - 1)) {
-							cursor_y++;
-						}
-						break;
 					case SDLK_LEFT:
-						if (cursor_x > 0) {
-							cursor_x--;
-						}
-						break;
 					case SDLK_RIGHT:
-						if (cursor_x < (CANVAS_XWIDTH - 1)) {
-							cursor_x++;
-						}
+						process_arrow_key(&cursor_x, &cursor_y, event);
 						break;
 					case SDLK_r:
-						user_tool->r = 255;
-						user_tool->g = 0;
-						user_tool->b = 0;
-						break;
 					case SDLK_k:
-						user_tool->r = 0;
-						user_tool->g = 0;
-						user_tool->b = 0;
-						break;
 					case SDLK_g:
-						user_tool->r = 0;
-						user_tool->g = 255;
-						user_tool->b = 0;
-						break;
 					case SDLK_b:
-						user_tool->r = 0;
-						user_tool->g = 0;
-						user_tool->b = 255;
-						break;
 					case SDLK_w:
-						user_tool->r = 255;
-						user_tool->g = 255;
-						user_tool->b = 255;
+						process_color_key(user_tool, event);
 						break;
 					case SDLK_LEFTBRACKET:
 						user_tool->radius = max_of(user_tool->radius - 1, 0);
@@ -279,12 +344,12 @@ int main(int argc, char* argv[]) {
 						break;
 					case SDLK_s: // save
 						printf("Saving...\n");
-						save_as(canvas, filename);
+						save_as(canvas, SAVE_AS);
 						printf("Saved.\n");
 						break;
 					case SDLK_l:
 						printf("Loading...\n");
-						load_as(canvas, filename);
+						load_as(canvas, SAVE_AS);
 						printf("Loaded.\n");
 						break;
 					default:
@@ -300,13 +365,8 @@ int main(int argc, char* argv[]) {
 
 	}
 
-
 	SDL_FreeSurface(canvas);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
-
-	// SDL_Delay(s2000); // 2000 ms delay - like waiting for 2 seconds. 
-	 				 // this is to see something for a bit then clean up on the way out.
-
 }
